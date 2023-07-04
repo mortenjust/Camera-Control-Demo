550ea64690999f3c878b8e19a4c06ba2b49253e2
Morten Just <morten.just@genstart.dk>
2023-07-04 11:05:49 +0200

    Initial Commit

---
+++ Camera Control Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Camera Control Demo
+//
+//  Created by Morten Just on 7/4/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
